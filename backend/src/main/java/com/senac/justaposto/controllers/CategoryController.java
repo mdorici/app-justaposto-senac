874f9cc5bbbfabee01b3ad799efa7f853754781f
mdorici <marianadorici@gmail.com>
2023-11-19 20:10:23 -0300

    POST new Category

--- backend/src/main/java/com/senac/justaposto/controllers/CategoryController.java
+++ backend/src/main/java/com/senac/justaposto/controllers/CategoryController.java
@@ -1,13 +1,17 @@
 package com.senac.justaposto.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.senac.justaposto.dto.CategoryDTO;
 import com.senac.justaposto.services.CategoryService;
@@ -33,5 +37,15 @@ public class CategoryController {
 		CategoryDTO dto = service.findById(id);
 		
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
