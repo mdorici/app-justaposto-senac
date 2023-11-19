95b267ce593e0257c4efe3c259938c0da91a6da6
mdorici <marianadorici@gmail.com>
2023-11-19 14:05:14 -0300

    Backend project created

---
+++ backend/src/main/java/com/senac/justaposto/JustapostoApplication.java
@@ -0,0 +1,13 @@
+package com.senac.justaposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustapostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustapostoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/senac/justaposto/JustapostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.justaposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustapostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
