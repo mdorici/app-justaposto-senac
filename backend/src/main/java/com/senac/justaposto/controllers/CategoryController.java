313e97e038e33c382e501f657f5ed071d0d167bf
mdorici <marianadorici@gmail.com>
2023-11-19 18:07:36 -0300

    DTO

--- backend/src/main/java/com/senac/justaposto/controllers/CategoryController.java
+++ backend/src/main/java/com/senac/justaposto/controllers/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.senac.justaposto.entities.Category;
+import com.senac.justaposto.dto.CategoryDTO;
 import com.senac.justaposto.services.CategoryService;
 
 @RestController
@@ -19,9 +19,9 @@ public class CategoryController {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		
-		List<Category> list = service.findAll();
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
--- backend/src/main/java/com/senac/justaposto/services/CategoryService.java
+++ backend/src/main/java/com/senac/justaposto/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.senac.justaposto.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.senac.justaposto.dto.CategoryDTO;
 import com.senac.justaposto.entities.Category;
 import com.senac.justaposto.repositories.CategoryRepository;
 
@@ -14,8 +17,11 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
-	public List<Category> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 }
