6b88afc69247077e6fb0abffa082a979e81eaa34
mdorici <marianadorici@gmail.com>
2023-11-19 22:21:26 -0300

    Pagination

--- backend/src/main/java/com/senac/justaposto/services/CategoryService.java
+++ backend/src/main/java/com/senac/justaposto/services/CategoryService.java
@@ -1,10 +1,10 @@
 package com.senac.justaposto.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 //import org.springframework.dao.DataIntegrityViolationException;
 //import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -24,10 +24,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);	
 		
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)
