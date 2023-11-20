bfd1de58b1eb192af37147aa57997cb139aae0eb
mdorici <marianadorici@gmail.com>
2023-11-20 12:13:51 -0300

    Custom ContraintValidator

--- backend/src/main/java/com/senac/justaposto/dto/UserInsertDTO.java
+++ backend/src/main/java/com/senac/justaposto/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.senac.justaposto.dto;
 
+import com.senac.justaposto.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
